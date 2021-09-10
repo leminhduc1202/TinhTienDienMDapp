cd0b94e4bf2a04b41b2129509ddf3bcab2a8accb
leminhduc1202 <duc.leminh.dn@gmail.com>
2021-09-10 15:34:33 +0700

    Update giao diện, tính năng chia sẻ, thay logo.

---
+++ app/src/main/java/mdideas/devapp/tinhtiendienmdapp/model/DetailViewModel.kt
@@ -0,0 +1,22 @@
+package mdideas.devapp.tinhtiendienmdapp.model
+
+import androidx.lifecycle.ViewModel
+
+
+private const val PRICE_BAC1 = 1678
+private const val PRICE_BAC2 = 1734
+private const val PRICE_BAC3 = 2014
+private const val PRICE_BAC4 = 2536
+private const val PRICE_BAC5 = 2834
+private const val PRICE_BAC6 = 2927
+//        donGiaBac1: Int = 1678
+//        donGiaBac2: Int = 1734
+//        donGiaBac3: Int = 2014
+//        donGiaBac4: Int = 2536
+//        donGiaBac5: Int = 2834
+//        donGiaBac6: Int = 2927
+class DetailViewModel: ViewModel() {
+
+}
+
+
